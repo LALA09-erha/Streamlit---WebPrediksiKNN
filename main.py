8cf1ca578415f70ff53dad40134e252bf0e25391
erha <fikri41153.pesonaxii@gmail.com>
2022-11-06 16:06:20 +0700

    update file main.py

--- main.py
+++ main.py
@@ -5,9 +5,34 @@ import time
 
 # pige title
 st.set_page_config(
-   page_title="Prediksi Penyakit Jantung",
     page_icon=":dolphin:",
 )
+
+def set_page_title(title):
+    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
+        <iframe height=0 srcdoc="<script>
+            const title = window.parent.document.querySelector('title') \
+                
+            const oldObserver = window.parent.titleObserver
+            if (oldObserver) {{
+                oldObserver.disconnect()
+            }} \
+
+            const newObserver = new MutationObserver(function(mutations) {{
+                const target = mutations[0].target
+                if (target.text !== '{title}') {{
+                    target.text = '{title}'
+                }}
+            }}) \
+
+            newObserver.observe(title, {{ childList: true }})
+            window.parent.titleObserver = newObserver \
+
+            title.text = '{title}'
+        </script>" />
+    """)
+    
+set_page_title("Prediksi Penyakit Jantung") 
 
 
 # hide menu
@@ -15,6 +40,9 @@ hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
+section.css-1vencpc.e1fqkh3o11 {min-width: 0px; width:0px !important;}
+.css-1vencpc {min-width: 0px; width:0px !i;}
+.e1fqkh3o11 {min-width: 0px; width:0px;}
 </style>
 
 """
