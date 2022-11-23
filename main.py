82b1ca45b738ee922c2eab1be8e5f597ce33844c
erha <fikri41153.pesonaxii@gmail.com>
2022-11-23 20:19:27 +0700

    update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ st.markdown(' <div style="position: fixed; top: 0; left: 0; z-index: 9999; width
 
 # insialisasi web
 kolom = st.columns((2, 0.48, 2.7))
-home = kolom[1].button('Main')
-about = kolom[2].button('About')
+home = kolom[1].button('🏠 Main')
+about = kolom[2].button('📰 About')
 
 # home page
 if home==False and about==False or home==True and about==False:
